9d5c97ae5ea43cd58a665be8736891ee019d02a6
Max Müller <43960767+MaxxxikLMAO@users.noreply.github.com>
2021-09-13 15:47:30 +0200

    Add files via upload

---
+++ Ryba.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ryba {
+    public static void main(String args[]) {
+
+        int decision;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Kolik ryb si das k veceri? ");
+        decision = sc.nextInt();
+
+        System.out.println("rybnicek 😳😳😳");
+        for (int x = 0; x < decision; x++) {
+            System.out.println("<° )))-<");
+        }
+    }
+}
